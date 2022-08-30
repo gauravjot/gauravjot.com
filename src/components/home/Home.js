8b4e09201feca4a06a40f39471d9e455bdafaf3e
Gauravjot Garaya <gauravjot@gmail.com>
2022-08-29 21:28:37 -0700

    skills boolean to show or not

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -4,6 +4,7 @@ import Footer from './Footer';
 import ProjectsOverview from './project/ProjectsOverview';
 import EducationOverview from './education/EducationOverview';
 import WorkExperience from './work/WorkExperience';
+import SkillOverview from './skill/SkillOverview';
 import Blog from './Blog';
 
 function Home() {
@@ -21,6 +22,7 @@ function Home() {
           <div className='col-sm-12 col-md-6'>
             <WorkExperience />
             <EducationOverview />
+            <SkillOverview />
           </div>
         </div>
       </div>
--- src/components/home/skill/SkillOverview.js
+++ src/components/home/skill/SkillOverview.js
@@ -9,39 +9,48 @@ export default function SkillOverview() {
     axios
       .get('https://gauravjot-portfolio.firebaseio.com/skill.json')
       .then(({ data }) => {
-        let projectsArray = [];
-        let learningsArray = [];
-        const projectsInJSON = data.skills.split(','); // project1, project2 ...
-        projectsInJSON.forEach(element => {
-          projectsArray.push(data[element]);
-        });
-        setSkills(projectsArray);
-        if (data.learning) {
-          data.learning.split(',').forEach(element => {
-            learningsArray.push(data[element]);
+        if (data.showSkills) {
+          let skillsArray = [];
+          const skillsInJSON = data.skills.split(','); // skill1, skill2 ..
+          skillsInJSON.forEach(element => {
+            skillsArray.push(data[element]);
           });
+          setSkills(skillsArray);
         }
-        setLearnings(learningsArray);
+        if (data.showLearning) {
+          let learningsArray = [];
+          if (data.learning) {
+            data.learning.split(',').forEach(element => {
+              learningsArray.push(data[element]);
+            });
+          }
+          setLearnings(learningsArray);
+        }
       });
   }, []);
 
   return (
     <div>
-      <div className='heading b m-b-15 m-t-15'>
-        <span className='b-accent-border'>I work with</span>
-      </div>
-      <div>
-        {skills.map(skill => (
-            <span>
-              {(skill.link) ?
-                <a href={skill.link} className='btn btn-primary btn-accent hover-underline' rel='noopener noreferrer'
-                target='_blank'>{skill.name}</a>
-                :
-                <span className='btn btn-primary btn-accent'>{skill.name}</span>
-              }
-            </span>
-        ))}
-      </div>
+      {(skills.length > 0) &&
+        <div>
+          <div className='heading b m-b-15 m-t-15'>
+            <span className='b-accent-border'>I work with</span>
+          </div>
+          <div>
+            {skills.map(skill => (
+                <span>
+                  {(skill.link) ?
+                    <a href={skill.link} className='btn btn-primary btn-accent hover-underline' rel='noopener noreferrer'
+                    target='_blank'>{skill.name}</a>
+                    :
+                    <span className='btn btn-primary btn-accent'>{skill.name}</span>
+                  }
+                </span>
+            ))}
+          </div>
+        </div>
+      }
+      
       {(learnings.length > 0) &&
         <div>
           <div className='heading b m-b-15 m-t-15'>
