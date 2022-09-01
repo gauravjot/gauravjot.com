d3c1c99df7c16c1bac6f66b75e455f1b179e456d
Gauravjot Garaya <gauravjot@gmail.com>
2022-08-31 23:51:51 -0700

    removed profile picture

--- src/components/home/Header.js
+++ src/components/home/Header.js
@@ -25,75 +25,63 @@ export default function Header() {
               <a className="nav-item nav-link" href="#education">Education</a>
             </div>
         </nav>
-        <div className='row'>
-          <div className='col-lg-3 col-md-4 col-sm-12'>
-            <center>
-              <img
-                src={profile[1]}
-                alt='Gauravjot Garaya'
-                className='img-fluid img-profile'
-              />
-            </center>
-            <div className='p-2' />
+        <div>
+          <div className='hello b'>Hello,</div>
+          <div className='statement'>
+            I'm <span className='name-author'>Gauravjot Garaya</span>
           </div>
-          <div className='col-lg-9 col-md-8 col-sm-12'>
-            <div className='hello b'>Hello,</div>
-            <div className='statement'>
-              I'm <span className='name-author'>Gauravjot Garaya</span>
+          {profile[2] &&
+            <div className='title'>
+              {profile[2]}
             </div>
-            {profile[2] &&
-              <div className='title'>
-                {profile[2]}
+          }
+          <div className='quick-about'>
+            <p>
+              {/* {profile[0] &&
+                <span>
+                  {profile[0]}
+                </span>
+              } */}
+              <div className='p-2' />
+              <div className='contact-links'>
+                <a
+                  href='https://github.com/gauravjot'
+                  className='cl-git'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                  title="Github"
+                >
+                  <i className='fab fa-lg fa-github' />
+                </a>
+                <a
+                  href='https://linkedin.com/in/gauravjot'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                  className='cl-linkedin'
+                  title="LinkedIn"
+                >
+                  <i className='fab fa-lg fa-linkedin' />
+                </a>
+                <a
+                  href='mailto:connect@gauravjot.com'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                  className='cl-email'
+                  title="Email"
+                >
+                  <i className='fas fa-lg fa-envelope' />
+                </a>
+                <a
+                  href='https://medium.com/@gauravjot'
+                  rel='noopener noreferrer'
+                  target='_blank'
+                  className='cl-medium'
+                  title="Medium"
+                >
+                  <i className='fa-brands fa-medium' />
+                </a>
               </div>
-            }
-            <div className='quick-about'>
-              <p>
-                {/* {profile[0] &&
-                  <span>
-                    {profile[0]}
-                  </span>
-                } */}
-                <div className='p-2' />
-                <div className='contact-links'>
-                  <a
-                    href='https://github.com/gauravjot'
-                    className='cl-git'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    title="Github"
-                  >
-                    <i className='fab fa-lg fa-github' />
-                  </a>
-                  <a
-                    href='https://linkedin.com/in/gauravjot'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    className='cl-linkedin'
-                    title="LinkedIn"
-                  >
-                    <i className='fab fa-lg fa-linkedin' />
-                  </a>
-                  <a
-                    href='mailto:connect@gauravjot.com'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    className='cl-email'
-                    title="Email"
-                  >
-                    <i className='fas fa-lg fa-envelope' />
-                  </a>
-                  <a
-                    href='https://medium.com/@gauravjot'
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    className='cl-medium'
-                    title="Medium"
-                  >
-                    <i className='fa-brands fa-medium' />
-                  </a>
-                </div>
-              </p>
-            </div>
+            </p>
           </div>
         </div>
       </div>
