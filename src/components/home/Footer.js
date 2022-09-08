d5fd331998b4f2067eb774646e121df481b9516b
Gauravjot Garaya <gauravjot@gmail.com>
2022-09-07 23:30:25 -0700

    design changes

--- src/components/home/Footer.js
+++ src/components/home/Footer.js
@@ -2,14 +2,16 @@ import React from 'react';
 
 export default function Footer() {
   return (
-    <center>
-      <div className='p-4' />
-      <div className='text-muted'>
-        Developed with <a href='https://reactjs.org' className='hover-underline'>React</a>
-        &nbsp;by Gauravjot Garaya, {new Date().getFullYear()}.
-        Open-source <a href='https://github.com/gauravjot/gauravjot.com' className='hover-underline'>GitHub</a>.
-      </div>
-      <br/>
-    </center>
+    <section className='footer'>
+      <center>
+        <div className='p-4' />
+        <div className='text-muted'>
+          Developed with <a href='https://reactjs.org' className='hover-underline'>React</a>
+          &nbsp;by Gauravjot Garaya, {new Date().getFullYear()}.
+          Open-source <a href='https://github.com/gauravjot/gauravjot.com' className='hover-underline'>GitHub</a>.
+        </div>
+        <br/>
+      </center>
+    </section>
   );
 }
